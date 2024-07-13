74c45ff7130f8eab15111cfa1b14fb0aef1c87d6
shikhar-proctur <shikharkumar.hbtu@gmail.com>
2024-07-13 17:49:57 +0530

    app navigater

--- src/navigations/AppNavigator.js
+++ src/navigations/AppNavigator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Image, StyleSheet} from 'react-native';
+import {Image, StyleSheet, Text} from 'react-native';
 
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
@@ -19,18 +19,23 @@ const Stack = createStackNavigator();
 const Drawer = createDrawerNavigator();
 const Tab = createBottomTabNavigator();
 
-const TabIcon = ({source}) => (
-  <Image source={source} style={styles.icon} />
-);
+const TabIcon = ({source}) => <Image source={source} style={styles.icon} />;
 
 const BottomTabNavigator = () => (
-  <Tab.Navigator>
+  <Tab.Navigator
+    screenOptions={{
+      tabBarLabelStyle: styles.tabBarLabelStyle,
+      tabBarIconStyle: styles.tabBarIconStyle,
+      tabBarStyle: styles.tabBarStyle,
+    }}>
     <Tab.Screen
       name="Home"
       component={DashboardScreen}
       options={{
         headerShown: false,
-        tabBarIcon: ({color, size}) => <TabIcon source={DashboardScreenImg.HomeButton} />,
+        tabBarIcon: ({color, size}) => (
+          <TabIcon source={DashboardScreenImg.HomeButton} />
+        ),
       }}
     />
     <Tab.Screen
@@ -38,7 +43,9 @@ const BottomTabNavigator = () => (
       component={DashboardScreen}
       options={{
         headerShown: false,
-        tabBarIcon: ({color, size}) => <TabIcon source={DashboardScreenImg.StackingButton} />,
+        tabBarIcon: () => (
+          <TabIcon source={DashboardScreenImg.StackingButton} />
+        ),
       }}
     />
     <Tab.Screen
@@ -46,20 +53,12 @@ const BottomTabNavigator = () => (
       component={DashboardScreen}
       options={{
         headerShown: false,
-        tabBarIcon: ({color, size}) => <TabIcon source={DashboardScreenImg.StackingButton} />,
+        tabBarIcon: () => (
+          <TabIcon source={DashboardScreenImg.StackingButton} />
+        ),
       }}
     />
   </Tab.Navigator>
-);
-
-const DrawerScreen = ({name, component, iconSource}) => (
-  <Drawer.Screen
-    name={name}
-    component={component}
-    options={{
-      drawerIcon: ({color, size}) => <Image source={iconSource} style={styles.image} />,
-    }}
-  />
 );
 
 const DrawerNavigator = () => (
@@ -68,97 +67,124 @@ const DrawerNavigator = () => (
     drawerContent={props => <CustomDrawerContent {...props} />}
     screenOptions={{
       drawerLabelStyle: styles.drawerLabelStyle,
-    }}
-  >
+    }}>
     <Drawer.Screen
       name="Dashboard"
       component={BottomTabNavigator}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.dashIcon} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.dashIcon} style={styles.image} />
+        ),
+        headerTitle: "My Dashboard", // Customize the header title
+        // headerRight: () => <Text style={{color: '#fff'}}>Right Item</Text>, // Customize the header right component
       }}
     />
     <Drawer.Screen
       name="Fiat Currencies"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.fiet} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.fiet} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Settings"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.setting} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.setting} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Block Matching Dividend"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.dividend} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.dividend} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Staking Referral Dividends"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.refferal} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.refferal} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="My Referral Code"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.refCode} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.refCode} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Security"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.security} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.security} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Currency Calculator"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.calculator} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.calculator} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Common Function For Vip"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.commanFunction} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.commanFunction} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Help And Support"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.shareApp} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.shareApp} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Terms and Conditions"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.terms} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.terms} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Logout"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.logout} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.logout} style={styles.image} />
+        ),
       }}
     />
     <Drawer.Screen
       name="Share App"
       component={HomeScreen}
       options={{
-        drawerIcon: ({color, size}) => <Image source={DrawerImages.shareApp} style={styles.image} />,
+        drawerIcon: () => (
+          <Image source={DrawerImages.shareApp} style={styles.image} />
+        ),
       }}
     />
   </Drawer.Navigator>
@@ -167,20 +193,55 @@ const DrawerNavigator = () => (
 const AppNavigator = () => (
   <NavigationContainer>
     <Stack.Navigator initialRouteName="Splash">
-      <Stack.Screen name="Splash" component={SplashScreen} options={{headerShown: false}} />
-      <Stack.Screen name="Login" component={LoginScreen} options={{headerShown: false}} />
-      <Stack.Screen name="Signup" component={SignupScreen} options={{headerShown: false}} />
-      <Stack.Screen name="Home" component={DrawerNavigator} options={{headerShown: false}} />
-      <Stack.Screen name="Stacking" component={StackingScreen} options={{headerShown: false}} />
+      <Stack.Screen
+        name="Splash"
+        component={SplashScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Signup"
+        component={SignupScreen}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Home"
+        component={DrawerNavigator}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Stacking"
+        component={StackingScreen}
+        options={{headerShown: false}}
+      />
     </Stack.Navigator>
   </NavigationContainer>
 );
 
 const styles = StyleSheet.create({
+  tabBarLabelStyle: {
+    fontSize: 14,
+    fontWeight: '400',
+    fontFamily: 'Montserrat',
+    paddingBottom: 5,
+    marginBottom: 8,
+  },
+  tabBarStyle: {
+    backgroundColor: '#F4F4F4',
+    height: 70,
+  },
+  tabBarIconStyle: {
+    width: 24,
+    height: 24,
+    resizeMode: 'contain',
+  },
   icon: {
     width: 24,
     height: 24,
-    marginBottom: 4,
   },
   image: {
     height: 24,
